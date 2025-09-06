5dee5479835a1095ee65141c3adec30c6fe033cf
Ali Eltahan <alyaltahan@gmail.com>
2025-09-06 10:52:58 +0300

    User/Reports Modules & DB Init

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Report } from './reports/reports.entity';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
