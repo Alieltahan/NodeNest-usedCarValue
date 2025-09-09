d11274718b8a0a8a5712bfe7760ac21f03c4bb2b
Alieltahan <alyaltahan@gmail.com>
2025-09-09 12:55:40 +0300

    Handle SignIn User

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -10,28 +10,31 @@ export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
   async signup(email: string, password: string) {
-    // see if email is in use
     const user = await this.usersService.find(email);
-    // if it is throw an error
     if (user.length) {
       throw new BadRequestException('email in use');
     }
-    // Hash the users password
-    // How should I hash the password via nestjs?
-    // Generate a Salt using randomBytes...
     const salt = randomBytes(8).toString('hex');
 
-    // const salt = await bcrypt.genSalt();
-    // Hash the salt and the password together
     const hash = (await asyncScrypt(password, salt, 32)) as Buffer;
-    // Store the salt and the hash in the password column
 
     const result = salt + '.' + hash.toString('hex');
 
     return this.usersService.createUser(email, result);
-    // create a new user and save it
-    // return the user
   }
 
-  signIn() {}
+  async signIn(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new BadRequestException('Email or password is incorrect');
+    }
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await asyncScrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Email or password is incorrect');
+    }
+
+    return user;
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,6 +18,11 @@ export class UsersController {
     return this.authService.signup(body.email, body.password);
   }
 
+  @Post('/signin')
+  signIn(@Body() body: CreateUserDto) {
+    return this.authService.signIn(body.email, body.password);
+  }
+
   @Get('/:id')
   findUserById(@Param('id') id: string) {
     return this.userService.findOne(parseInt(id));
